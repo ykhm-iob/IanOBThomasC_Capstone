24edb89372fc744003f84fdb6c02d254ca0e23d0
Ian O'Brien <IOBRIE01@rams.shepherd.edu>
2024-03-28 00:43:16 -0400

    Some light changes, learning React. Not much real work on the project yet.

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Ed Steers Jr.'s Site</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/books">Books Page</a>
+        <a href="/about_me">About Me</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
