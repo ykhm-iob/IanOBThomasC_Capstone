ec0896440716517750af162a4499e032af5837fc
Ian O'Brien <IOBRIE01@rams.shepherd.edu>
2024-04-01 17:27:35 -0400

    useFetch now catches AbortError.

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,7 +6,8 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(url)
+    const abortCont = new AbortController();
+    fetch(url, { signal: abortCont.signal })
       .then(res => {
         if(!res.ok) {
           throw Error('Could Not Fetch Page Data!');
@@ -19,9 +20,15 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch(err => {
-        setIsPending(false);
-        setError(err.message);
-      })
+        if (err.name === 'AbortError') {
+          console.log('Fetch Aborted.')
+        } else {
+          setIsPending(false);
+          setError(err.message); 
+        }
+      });
+
+    return () => abortCont.abort();
   }, [url]);
 
   return { data, isPending, error }
