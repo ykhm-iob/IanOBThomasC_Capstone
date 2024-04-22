5e2bea055019c6c4b469654aea68e214a2d517bb
Ian O'Brien <IOBRIE01@rams.shepherd.edu>
2024-04-22 00:00:47 -0400

    Added line height and letter spacing to bodies of text. Some padding adjustments.

--- src/About.js
+++ src/About.js
@@ -4,26 +4,28 @@ const About = () => {
   return ( 
     <div className="flex flex-col gap-36 text-center">
       <div>
-        <h2 className="text-titleColor text-3xl font-medium lg:text-5xl">About Me</h2>
-        <p className="text-paragraphColor text-sm lg:text-lg">Dr. Steers was trained at University of PA to be a molecular biologist in 1963,<br /> graduating with a PhD from the University of Pennsylvania. He would go on to 
-        <br /> become a biomedical researcher in collobration with a Nobel Prize winner, before retiring 
-        <br /> as a history writer. It is here where Dr. Steers would release multiple books, and have 
-        <br />multiple guest appearances on radio and TV shows...</p>
+        <h2 className="text-titleColor text-3xl font-medium pt-5 pb-3 lg:pb-5 lg:text-5xl">About Me</h2>
+        <p className="text-paragraphColor tracking-wide	leading-relaxed text-ellipsis text-sm max-w-sm px-10
+        sm:max-w-2xl sm:text-base md:max-w-2xl lg:text-lg lg:max-w-4xl mx-auto line-clamp-6 
+        md:line-clamp-none hover:line-clamp-none">Dr. Steers was trained at University of PA to be a molecular biologist in 1963, graduating with a PhD from the University of Pennsylvania. He would go on to 
+         become a biomedical researcher in collobration with a Nobel Prize winner, before retiring 
+         as a history writer. It is here where Dr. Steers would release multiple books, and have 
+        multiple guest appearances on radio and TV shows...</p>
         <Link to="/about/Biography"><button className="button">Biography Page</button></Link>
       </div>
       <div>
-        <h2 className="text-titleColor text-3xl font-medium lg:text-5xl">Appearances</h2>
-        <p className="text-paragraphColor text-sm lg:text-lg">Dr. Steers has had multiple appearances over the years, including but not limited to:</p>
-        <ul>
-          <li>Over 13 televised appearances, including educational and radio programs.</li>
-          <li>Multiple interviews, including one on The Today Show.</li>
-          <li>His book(s) discussed on CNN.</li>
-        </ul>
+        <h2 className="text-titleColor text-3xl font-medium pb-3 lg:pb-5 lg:text-5xl">Appearances</h2>
+        <p className="text-paragraphColor tracking-wide	leading-relaxed text-ellipsis text-sm max-w-sm px-10
+        sm:max-w-2xl sm:text-base md:max-w-2xl lg:text-lg lg:max-w-4xl mx-auto line-clamp-6 
+        md:line-clamp-none hover:line-clamp-none">Dr. Steers has had multiple appearances over the years, including but not limited to:
+        <br /> Over 13 televised appearances, including educational and radio programs.
+        <br /> Multiple interviews, including one on The Today Show.
+        <br /> His book(s) discussed on CNN. </p>
         <Link to="/about/Appearances"><button className="button">Appearances Page</button></Link>
       </div>
       <div>
-        <h2 className="text-titleColor text-3xl font-medium lg:text-5xl">Contact Info</h2>
-        <p className="text-paragraphColor text-sm lg:text-lg">Send me an email at: ed@edsteers.com</p>
+        <h2 className="text-titleColor text-3xl font-medium pb-3 lg:pb-5 lg:text-5xl">Contact Info</h2>
+        <p className="text-paragraphColor tracking-wide leading-relaxed text-sm lg:text-lg">Send me an email at: ed@edsteers.com</p>
         {/* <button className="button mb-4"><Link to="/about/Contact">Contact Me</Link></button> */}
       </div>
     </div>
--- src/BookInfo.js
+++ src/BookInfo.js
@@ -30,13 +30,15 @@ const BookInfo = () => {
       { book && (
         <article className="flex flex-col gap-10 text-center">
           <h2 className="text-titleColor text-3xl font-medium lg:text-5xl">Book Info</h2>
-          <h2 className="text-titleColor text-sm max-w-sm text-3xl font-medium lg:text-5xl
-          sm:max-w-2xl sm:text-base md:max-w-2xl lg:text-lg lg:max-w-4xl">{ book.title }</h2>
+          <h2 className="text-titleColor mx-auto text-2xl max-w-sm text-3xl font-medium
+          sm:max-w-2xl md:max-w-2xl lg:text-5xl lg:max-w-4xl">{ book.title }</h2>
           <img className="rounded-2xl w-2/6 min-w-36 mx-auto m-4 md:max-w-56 lg:max-w-64" src={ book.cover_img } alt="Book Cover" /> {/* {images[book.cover]} */}
-          <p className="text-paragraphColor text-ellipsis text-sm max-w-sm px-10 
+          <p className="text-paragraphColor text-ellipsis tracking-wide leading-relaxed text-sm max-w-sm px-10 
+        tracking-wide leading-relaxed
         sm:max-w-2xl sm:text-base md:max-w-2xl lg:text-lg lg:max-w-4xl mx-auto 
         line-clamp-5 md:line-clamp-none hover:line-clamp-none"><b>Synopsis:</b> {book.description }</p>
-          <p className="text-paragraphColor text-ellipsis text-sm max-w-sm px-10 
+          <p className="text-paragraphColor text-ellipsis text-sm max-w-sm px-10
+          tracking-wide leading-relaxed 
         sm:max-w-2xl sm:text-base md:max-w-2xl lg:text-lg lg:max-w-4xl mx-auto"><b>Written By:</b> { book.author }</p>
           <a href={ book.url } target="_blank" rel="noreferrer"><button className="button">Amazon Page</button></a>
           <Link to="/books"><button className="button mb-4">Return To Books Page</button></Link>
